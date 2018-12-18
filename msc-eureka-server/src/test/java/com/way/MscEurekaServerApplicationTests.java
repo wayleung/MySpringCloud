2c9cffe56296853b1e626ed109006e65c59bde06
wayleung <610686949@qq.com>
2018-12-18 17:57:13 +0800

    add msc-eureka-server

---
+++ msc-eureka-server/src/test/java/com/way/MscEurekaServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.way;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MscEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

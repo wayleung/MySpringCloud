0c25636349e8d390506dc78e957d941a48023396
wayleung <610686949@qq.com>
2018-12-23 21:45:17 +0800

    在hiService方法上加上@HystrixCommand注解     该注解对该方法创建了熔断器的功能，     并指定了fallbackMethod熔断方法，     熔断方法直接返回了一个字符串

--- msc-ribbon-rest/src/main/java/com/way/MscRibbonRestApplication.java
+++ msc-ribbon-rest/src/main/java/com/way/MscRibbonRestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 //@EnableDiscoveryClient
 /*
 spring cloud中discovery service有许多种实现（eureka、consul、zookeeper等等），
--- msc-ribbon-rest/src/main/java/com/way/service/HelloService.java
+++ msc-ribbon-rest/src/main/java/com/way/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.way.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,7 +16,18 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    /*
+    在hiService方法上加上@HystrixCommand注解
+    该注解对该方法创建了熔断器的功能，
+    并指定了fallbackMethod熔断方法，
+    熔断方法直接返回了一个字符串
+     */
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name){
         return restTemplate.getForObject("http://msc-eureka-client/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name){
+        return "hi,"+name+",sorry,error!";
     }
 }

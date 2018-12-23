4d2f8677060809a736c379b009e34ccec317ad5e
wayleung <610686949@qq.com>
2018-12-23 22:11:50 +0800

    add msc-zuul dependency

---
+++ msc-zuul/src/main/java/com/way/MscZuulApplication.java
@@ -0,0 +1,14 @@
+package com.way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscZuulApplication.class, args);
+    }
+
+}
+

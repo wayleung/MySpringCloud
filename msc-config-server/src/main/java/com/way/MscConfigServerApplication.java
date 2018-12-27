f4a66fdacbe89b6680880e5469eb21bf09256998
wayleung <610686949@qq.com>
2018-12-27 17:54:27 +0800

    add config-client

--- msc-config-server/src/main/java/com/way/MscConfigServerApplication.java
+++ msc-config-server/src/main/java/com/way/MscConfigServerApplication.java
@@ -3,14 +3,21 @@ package com.way;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class MscConfigServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MscConfigServerApplication.class, args);
     }
 
+    @GetMapping(value = "/hi")
+    public String hi(){
+        return "hi";
+    }
 }
 

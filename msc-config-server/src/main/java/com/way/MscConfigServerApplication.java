16a7b2dc8e4002b81c9a862577baf964c9d19f5a
wayleung <610686949@qq.com>
2018-12-27 17:29:11 +0800

    spring.cloud.config.server.git.uri：配置git仓库地址 spring.cloud.config.server.git.searchPaths：配置仓库路径 spring.cloud.config.label：配置仓库的分支 spring.cloud.config.server.git.username：访问git仓库的用户名 spring.cloud.config.server.git.password：访问git仓库的用户密码 如果Git仓库为公开仓库，可以不填写用户名和密码，如果是私有仓库需要填写，本例子是公开仓库，放心使用。

---
+++ msc-config-server/src/main/java/com/way/MscConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MscConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscConfigServerApplication.class, args);
+    }
+
+}
+

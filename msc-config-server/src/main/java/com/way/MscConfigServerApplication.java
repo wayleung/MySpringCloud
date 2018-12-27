285e06f6e8b31e9eab59afce19cd68c8d525d6a2
wayleung <610686949@qq.com>
2018-12-27 18:04:41 +0800

    #这时发现，在读取配置文件不再写ip地址，而是服务名，这时如果配置服务部署多份，通过负载均衡，从而高可用。
    eureka.client.service-url.defaultZone=http://localhost:8761/eureka/
    #是从配置中心读取文件
    spring.cloud.config.discovery.enabled=true
    #配置中心的servieId，即服务名。
    spring.cloud.config.discovery.service-id=msc-config-server

--- msc-config-server/src/main/java/com/way/MscConfigServerApplication.java
+++ msc-config-server/src/main/java/com/way/MscConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.way;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableConfigServer
 @RestController
+@EnableDiscoveryClient
 public class MscConfigServerApplication {
 
     public static void main(String[] args) {

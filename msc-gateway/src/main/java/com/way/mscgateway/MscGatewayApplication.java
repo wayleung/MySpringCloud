19a8e5dd5ae3886dcadba5c3c87cea730f0d4b71
wayleung <610686949@qq.com>
2019-01-22 13:51:44 +0800

    add spring cloud gateway

---
+++ msc-gateway/src/main/java/com/way/mscgateway/MscGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.way.mscgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscGatewayApplication.class, args);
+    }
+
+}
+
---
+++ msc-gateway/src/test/java/com/way/mscgateway/MscGatewayApplicationTests.java
@@ -0,0 +1,17 @@
+package com.way.mscgateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MscGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+

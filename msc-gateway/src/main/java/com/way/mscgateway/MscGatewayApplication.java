6293918b5b9a9c0e79bab43d3ba95c63a6e81e22
wayleung <610686949@qq.com>
2019-01-22 15:12:34 +0800

    add msc-gateway

--- msc-gateway/src/main/java/com/way/mscgateway/MscGatewayApplication.java
+++ msc-gateway/src/main/java/com/way/mscgateway/MscGatewayApplication.java
@@ -2,12 +2,25 @@ package com.way.mscgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MscGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MscGatewayApplication.class, args);
+    }
+
+
+    public RouteLocator myRoutes(RouteLocatorBuilder builder){
+        return builder.routes().route(p->p
+                .path("/get")
+                .filters(f->f.addRequestHeader("Hello","World"))
+                .uri("http://httpbin.org:80"))
+                .build();
     }
 
 }

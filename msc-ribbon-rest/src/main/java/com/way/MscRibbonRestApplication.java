8c1967140148194414c909eb511db758b814ab65
wayleung <610686949@qq.com>
2019-01-22 13:44:17 +0800

    hystrix dashboard 1·<dependency> <groupId>org.springframework.boot</groupId> <artifactId>spring-boot-starter-actuator</artifactId> </dependency> <dependency> <groupId>org.springframework.cloud</groupId> <artifactId>spring-cloud-starter-netflix-hystrix</artifactId> </dependency> <dependency> <groupId>org.springframework.cloud</groupId> <artifactId>spring-cloud-starter-netflix-hystrix-dashboard</artifactId> </dependency>
    2·@EnableHystrixDashboard注解
    
    3·需要在hystrix dashboard的服务的application.properties文件中添加
    #hystrix dashboard
    management.endpoints.web.exposure.include=*
    management.endpoints.web.cors.allowed-origins=*
    management.endpoints.web.cors.allowed-methods=*
    
    4·在请求http://localhost:8764/actuator/hystrix.stream之前，需要随便请求一个接口，例如：http://localhost:8764/hi，否则会一直ping ping ping

--- msc-ribbon-rest/src/main/java/com/way/MscRibbonRestApplication.java
+++ msc-ribbon-rest/src/main/java/com/way/MscRibbonRestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableHystrixDashboard
 //@EnableDiscoveryClient
 /*
 spring cloud中discovery service有许多种实现（eureka、consul、zookeeper等等），

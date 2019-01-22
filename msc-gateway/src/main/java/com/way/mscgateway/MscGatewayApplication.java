f2d6d8a01cbb0a06ecf4566555ed09746fc1ca1f
wayleung <610686949@qq.com>
2019-01-22 16:32:11 +0800

    add route 2.使用Hystrix
    在spring cloud gateway中可以使用Hystrix。Hystrix是 spring cloud中一个服务熔断降级的组件，在微服务系统有着十分重要的作用。
    Hystrix是 spring cloud gateway中是以filter的形式使用的，代码如下：
    
    @Bean
    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
    String httpUri = "http://httpbin.org:80";
    return builder.routes()
    .route(p -> p
    .path("/get")
    .filters(f -> f.addRequestHeader("Hello", "World"))
    .uri(httpUri))
    .route(p -> p
    .host("*.hystrix.com")
    .filters(f -> f
    .hystrix(config -> config
    .setName("mycmd")
    .setFallbackUri("forward:/fallback")))
    .uri(httpUri))
    .build();
    }
    
    在上面的代码中，我们使用了另外一个router，该router使用host去断言请求是否进入该路由，当请求的host有“*.hystrix.com”，都会进入该router，该router中有一个hystrix的filter,该filter可以配置名称、和指向性fallback的逻辑的地址，比如本案例中重定向到了“/fallback”。
    
    现在写的一个“/fallback”的l逻辑：
    @RequestMapping("/fallback") public Mono<String> fallback() { return Mono.just("fallback"); }
    
    Mono是一个Reactive stream，对外输出一个“fallback”字符串。
    
    使用curl执行以下命令：
    
    curl --dump-header - --header 'Host: www.hystrix.com' http://localhost:8080/delay/3
    
    返回的响应为：
    
    fallback
    
    可见，带hostwww.hystrix.com的请求执行了hystrix的fallback的逻辑。
    
    总结
    本文通过官方的一个简单的案例，来讲解了spring cloud gateway的简单用法，在spring cloud gateway中有2个重要的概念predicates和filters，它们个将会在后续文章讲解。敬请期待。

--- msc-gateway/src/main/java/com/way/mscgateway/MscGatewayApplication.java
+++ msc-gateway/src/main/java/com/way/mscgateway/MscGatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -15,12 +16,44 @@ public class MscGatewayApplication {
     }
 
 
+    @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder){
-        return builder.routes().route(p->p
+        String httpUrl = "http://httpbin.org:80";
+
+        return builder.routes()
+                .route(p->p
                 .path("/get")
                 .filters(f->f.addRequestHeader("Hello","World"))
-                .uri("http://httpbin.org:80"))
+//                .uri("http://localhost:8761"))
+                .uri(httpUrl))
+
+                /**
+                 * 当我们向gateway工程请求“/get”,
+                 * gateway会将工程的请求转发到“http://httpbin.org/get”，
+                 * 并且在转发之前，加上一个filter，
+                 * 该filter会将请求添加一个header,
+                 * key为hello，value为world。
+                 */
+
+                .route(p->p
+                        .host("*.hystrix.com")
+                        .filters(f->f
+                                .hystrix(config -> config
+                                        .setName("mycmd")
+                                        .setFallbackUri("foward:/fallback")))
+                        .uri(httpUrl))
                 .build();
+
+        /**
+         *在上面的代码中，我们使用了另外一个router，
+         * 该router使用host去断言请求是否进入该路由，
+         * 当请求的host有“*.hystrix.com”，都会进入该router，
+         * 该router中有一个hystrix的filter,该filter可以配置名称、和指向性fallback的逻辑的地址，
+         * 比如本案例中重定向到了“/fallback”
+         */
+
+
+
     }
 
 }
